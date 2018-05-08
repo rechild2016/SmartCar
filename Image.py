d846cdd447645655366f265b5bf73a72cf8b609c
Rechild <1102530068@qq.com>
2018-05-08 19:48:22 +0800

    添加图片资源

--- Image.py
+++ Image.py
@@ -1,6 +1,7 @@
 import math
 import numpy as np
 import cv2
+from scipy import signal
 
 fontface = cv2.FONT_HERSHEY_SIMPLEX
 fontscale = 0.8
@@ -13,35 +14,23 @@ def ImageProcess(image, type):
 
     img3 = np.ones((60, 160)) * 255
     img4 = np.ones((60, 160)) * 255
-
+    img5 = np.ones((60, 160)) * 255
     _, contours, hierarchy = cv2.findContours(img2, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
 
-    c_max = []
+    contoursFliter = []
     for i in range(len(contours)):
         cnt = contours[i]
         area = cv2.contourArea(cnt)
         # print(len(contours[i]), area)
         # 处理掉小的轮廓区域，这个区域的大小自己定义。
         if (len(contours[i]) > 30 or area > 10):
-            c_max.append(cnt)
+            contoursFliter.append(cnt)
 
-    print("有效轮廓: ", len(c_max))
+    print("有效轮廓: ", len(contoursFliter))
 
-    cv2.drawContours(img3, c_max, -1, (0, 255, 0), 2)
+    cv2.drawContours(img3, contoursFliter, -1, (0, 255, 0), 2)
     cv2.drawContours(img4, contours, -1, (0, 100, 0), 2)
 
-<<<<<<< HEAD
-    mid = midLine(c_max)
-    for i in range(1, len(mid)):
-        cv2.line(img4,mid[i-1],mid[i],(0,255,0),thickness=2)
-
-    im1 = cv2.resize(im_gray, (200, 200))
-    im2 = cv2.resize(img3, (200, 200))
-    im3 = cv2.resize(img4, (200, 200))
-
-    cv2.putText(im3, type, (60, 60), fontface, fontscale, fontcolor)
-    return np.hstack((im1, im3, im2))
-=======
     mid = midLine(contoursFliter)
     for i in range(1, len(mid)):
         cv2.line(img4,mid[i-1],mid[i],(0,255,0),thickness=2)
@@ -52,7 +41,6 @@ def ImageProcess(image, type):
 
     cv2.putText(im2, type, (50, 90), fontface, fontscale, fontcolor)
     return np.hstack((im1, im2, im3))
->>>>>>> 3d4297d... 完成预期目标
 
 # def Least_squares(x,y):
 #     x_ = x.mean()
@@ -79,33 +67,15 @@ def midLine(contours):
             contourPoints[item[1]].append(item[0])
 
     mid = []
-<<<<<<< HEAD
-    for i in range(60):
-        ave=0
-=======
     mid.append((80,61))
     mid.append((80, 60))
     for i in range(60):
         ave = -1
->>>>>>> 3d4297d... 完成预期目标
         if len(contourPoints[59 -i]) > 1 and len(contourPoints[59 -i]) <= 6 :
             ave = math.ceil((max(contourPoints[59-i]) + min(contourPoints[59-i]))/2)
             if ( i >5 and math.fabs(ave - mid[-1][0]) > 10):
                 ave = 2*mid[-1][0] -mid[-2][0]
             mid.append((ave, 59 -i))
-<<<<<<< HEAD
-        print(i," :",contourPoints[59 -i], ave)
-
-    print(mid)
-    return mid
-
-# image = cv2.imread("./fig/Images/shizi/016.jpg")
-# image = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
-# im1 = ImageProcess(image,"shizi")
-#
-# cv2.imshow("img", im1)
-# cv2.waitKey(0)
-=======
         # print(i," :",contourPoints[59 -i], ave)
 
     # print(mid)
@@ -118,4 +88,3 @@ if __name__ == '__main__':
 
     cv2.imshow("img", im1)
     cv2.waitKey(0)
->>>>>>> 3d4297d... 完成预期目标
